7428deed3beef5e298331c93c9a2b08eb800569b
Amir Pourhadi <Alex.ce1379@gmail.com>
2021-05-08 00:51:42 +0430

    Amir-Answers

---
+++ script.js
@@ -0,0 +1,14 @@
+function slider(input, index) {
+	sliderIndex = index;
+	const item = document.querySelector(`#${input}`);
+	const slideArr = [...document.querySelector(".slides").children];
+	slideArr.forEach((element) => element.classList.remove("active"));
+	item.classList.add("active");
+}
+
+let slideIndex = 1;
+setInterval(() => {
+	slideIndex++;
+	if (slideIndex === 3) slideIndex = 1;
+	slider(`slide${slideIndex}`, slideIndex);
+}, 2000);
