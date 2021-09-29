87f71aaf5943c10c32f87735321b01a6961f855e
Amir Pourhadi <Alex.CE1379@Gmail.com>
2021-09-29 19:24:04 +0330

    indented with spaces

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 function slider(input, index) {
-	sliderIndex = index;
-	const item = document.querySelector(`#${input}`);
-	const slideArr = [...document.querySelector(".slides").children];
-	slideArr.forEach((element) => element.classList.remove("active"));
-	item.classList.add("active");
+  sliderIndex = index;
+  const item = document.querySelector(`#${input}`);
+  const slideArr = [...document.querySelector(".slides").children];
+  slideArr.forEach((element) => element.classList.remove("active"));
+  item.classList.add("active");
 }
 
 let slideIndex = 1;
 setInterval(() => {
-	slideIndex++;
-	if (slideIndex === 3) slideIndex = 1;
-	slider(`slide${slideIndex}`, slideIndex);
+  slideIndex++;
+  if (slideIndex === 3) slideIndex = 1;
+  slider(`slide${slideIndex}`, slideIndex);
 }, 2000);
