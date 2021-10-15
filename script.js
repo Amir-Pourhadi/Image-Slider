ec3e5d53673b9a6c7d5c576fe260257c233762f0
Amir Pourhadi <Alex.CE1379@Gmail.com>
2021-10-15 21:08:25 +0330

    extra variables removed

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
-function slider(input, index) {
-  sliderIndex = index;
+function slider(input) {
   const item = document.querySelector(`#${input}`);
   const slideArr = [...document.querySelector(".slides").children];
   slideArr.forEach((element) => element.classList.remove("active"));
@@ -9,6 +8,6 @@ function slider(input, index) {
 let slideIndex = 1;
 setInterval(() => {
   slideIndex++;
-  if (slideIndex === 3) slideIndex = 1;
-  slider(`slide${slideIndex}`, slideIndex);
+  if (slideIndex === 4) slideIndex = 1;
+  slider(`slide${slideIndex}`);
 }, 2000);
